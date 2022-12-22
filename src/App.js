ef8891086474fbd6e76b86f542cf0cd34d83fbb6
Susan Heller <*jefa8@gmail.com>
2022-12-21 19:10:59 -0500

    xss excercise

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ const handleSubmit = e => {
         image:""
       });
       // Launch first attack here
+      eval(task.text)
     }
   }
 
@@ -71,8 +72,10 @@ const handleSubmit = e => {
       ))}
     </>
         {/* Launch second attack here. */}
+        <div style={{"visibility": "hidden"}} dangerouslySetInnerHTML={{__html: task.image}} />
     </>
   )
 }
 
 export default App
+
